0762f9a3ad779a5172933a11defcf48333997f29
Andrew High <andrewhigh4@hotmail.com>
2021-05-12 08:43:05 +0100

    working on last e2e test

--- react_calculator/cypress/integration/App.spec.js
+++ react_calculator/cypress/integration/App.spec.js
@@ -15,4 +15,63 @@ describe("Calculator", () => {
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '4');
   })
+
+  it('should allow multiple operations to be chained together', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '6');
+  })
+
+  it('should work for negative numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-2');
+  })
+
+  it('should work for decimal numbers', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.5');
+  })
+
+  it('should work for very large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '9999999800000000');
+  })
+
+  it('should work for decimal numbers', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.5');
+  })
+
+  
 })
